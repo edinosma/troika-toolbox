ac3fa5c90d87a63e0e669c0a1cc67bc59f02e368
Edin O <edinosma@icloud.com>
2023-11-04 19:44:42 -0400

    Update MSD calculation program
    Get compMSD figured out, start work on fitMSD

--- msd/compMSD.m
+++ msd/compMSD.m
@@ -1,24 +1,35 @@
-function [msd] = compMSD(trjR, timelag)
-%COMPMSD Compute the MSD of a trjR.
+function [msd] = compMSD(trjR, timelag, asArray)
+%COMPMSD Compute the MSD of a trjR. Saved as [mean, std, dt]
 %   Ensemble-MSD(t) = <r>^2 (t) = <|(y(j)-y(0))|^2 + |(x(j) - x(0))|^2> 
 
 %% Allocation of memory
 [frames, dim, spots] = size(trjR);
 dim = dim - 1;
-msd = nan(frames-1, dim+1, spots);
 
-for iTraj = 1:spots
-    %% Get dR, find dT, save to msd(timeframe, [mean(dR.^2, 1), dT], particle number)
+temp = cell(spots, 1); % Due to how parfor works, we need to make MSD into a cell then flatten it at the end.
+[temp{:}] = deal([0,0,0; nan(frames-1, dim+1)]); % Initial MSD is 0 because r(0) - r(0) = 0.
+
+%% ParFor Loop
+parfor iTraj = 1:spots
     nonzs = reshape(nonzeros(trjR(:,1:dim,iTraj)), [], dim);
-    numNZ = size(nonzs,1);
-    atTime = diff(find(trjR(:,1,iTraj)) .* timelag); atTime = [0; atTime];
+    numNZ = size(nonzs,1) - 1;
+    atTime = (find(trjR(:,1,iTraj)) .* timelag);
 
     for jTraj = 1:numNZ
-        dR = [abs(nonzs(jTraj:end,1) - nonzs(1,1)), ...
-              abs(nonzs(jTraj:end,2) - nonzs(1,2))]; % dy, dx
+        dR2 = sum( (nonzs(1+jTraj:end, :) - nonzs(1:end-jTraj, :)).^ 2, 2); % dy, dx
 
-        msd(jTraj, :, iTraj) = [mean(dR.^2, 1), atTime(jTraj)]; % dy, dx, dt
+        msdCalc = [mean(dR2), std(dR2), atTime(jTraj)]; % mean, std, dt
+        
+        temp{iTraj}(jTraj+1, :) = msdCalc;
     end
+
+end
+
+%% Reshape Cell to Array
+if asArray
+    msd = reshape(cell2mat(temp), frames, [], spots);
+else
+    msd = temp;
 end
 
 end
--- msd/fitMSD.m
+++ msd/fitMSD.m
@@ -1,7 +1,18 @@
-function [outputArg1,outputArg2] = fitMSD(inputArg1,inputArg2)
-%FITMSD Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+% Fits MSD and log(MSD) to mx+b. Written by Edin Osmanbasic.
+TIME_LAG = 0.03;
 
+% msd = compMSD(trjR, TIME_LAG, false); % If you have an MSD variable generated, just comment this line out
+
+coeff = cell(size(msd));
+
+parfor iMSD = 1:size(msd, 1)
+    fitD   = fit(msd{iMSD}(:,1), msd{iMSD}(:,3), 'poly1'); % MSD = Dx + ...
+
+    msdLogd = log10(msd{iMSD});
+    msdLogd = reshape(msdLogd(~isinf(msdLogd)), [], 3);
+    fitAlp = fit(, 'poly1'); % log(MSD) = alpha*x + ...
+
+    coeffD   = coeffvalues(fitD);
+    coeffAlp = coeffvalues(fitAlp);
+    coeff{iMSD} = [coeffD(1), coeffAlp(1)];
+end
